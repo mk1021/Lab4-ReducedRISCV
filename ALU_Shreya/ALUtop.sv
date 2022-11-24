d2bb224185c86e5d7cac328b54ceb7a0d68673ef
shreyak02 <shreya02@outlook.com>
2022-11-24 14:16:29 +0000

    ALU changes

---
+++ ALU_Shreya/ALUtop.sv
@@ -0,0 +1,52 @@
+module ALUtop #(
+    parameter       A_WIDTH = 5,
+    parameter       D_WIDTH = 32
+)(
+    input logic [A_WIDTH-1:0]   rs1,
+    input logic [A_WIDTH-1:0]   rs2,
+    input logic [A_WIDTH-1:0]   rd,
+    input logic                 clk,
+    input logic                 RegWrite,
+    input logic [D_WIDTH-1:0]   ImmOp,
+    input logic [2:0]           ALUctrl,  
+    input logic                 ALUsrc,
+    output logic                EQ,
+    output logic [D_WIDTH-1:0]  a0
+
+);
+
+logic [D_WIDTH-1:0]             rd1 ; // wire connecting RD1 output to ALUop1
+logic [D_WIDTH-1:0]             rd2 ; // wire connecting RD2 output to multiplexer
+logic [D_WIDTH-1:0]             ALUop2 ; // wire connecting multiplexer into input of ALU
+logic [D_WIDTH-1:0]             ALUout ; // wire connecting output of ALU to regfile WD3
+
+
+regfile read_data1 (
+    .clk(clk),
+    //.rst(rst), need to include
+    .rd1(rd1),
+    .rs1(rs1),
+    .rs2(rs2),
+    .rd(rd),
+    .RegWrite(RegWrite),
+    .write_data3(ALUout),
+    .rd2(rd2), 
+    .a0(a0)
+);
+
+ALUsrc src(
+    .regOp2(rd2),
+    .ALUsrc(ALUsrc),
+    .ImmOp(ImmOp),
+    .ALUop2(ALUop2) // ALUop2
+);
+
+ALU alu(
+    .ALUctrl(ALUctrl),
+    .ALUop1(rd1),
+    .ALUop2(ALUop2), // ALUop2
+    .Sum(ALUout),
+    .EQ(EQ)
+);
+
+endmodule
---
+++ ALU_Shreya/regfile.sv
@@ -0,0 +1,44 @@
+module regfile#(
+    parameter A_WIDTH = 5,
+              D_WIDTH = 32
+)(
+    input logic  [A_WIDTH-1:0]       rs1, //AD1
+    input logic  [A_WIDTH-1:0]       rs2, //AD2
+    input logic [A_WIDTH-1:0]        rd,  //AD3
+    input logic                     clk,
+    input logic                     RegWrite, //WE3
+    input logic  [D_WIDTH-1:0]      write_data3,  //WD3
+    output logic [D_WIDTH-1:0]      rd1, //RD1
+    output logic [D_WIDTH-1:0]      rd2, //RD2
+    output logic [D_WIDTH-1:0]      a0 // reg10...return value is loaded here so the TB can inspect the state of the CPU (see value of a0)
+
+);
+
+//include rst 
+
+logic [D_WIDTH-1:0] mem_array [2**A_WIDTH-1:0];  //CHECK??????????????????? (32x32 bit reg?)
+
+
+//synchronus write
+always_ff @(posedge clk) begin 
+
+    if (RegWrite)
+        mem_array[rd] <= write_data3;
+end
+
+//asynchronus read
+always_comb 
+    begin
+        a0 = mem_array[5'd10]; //return value loaded here
+        //mem_array[0] = 0;
+        //rd1 = mem_array[rs1];
+        //rd2 = mem_array[rs2];
+    end
+
+always rd1=mem_array[rs1];
+always rd2=rs2;
+
+assign mem_array[0] = 0;
+assign mem_array[1] = 1;
+
+endmodule
